97674cda1de1733e6efe0c1606b1f3fdc38285ac
v-p-b <vpbalint@gmail.com>
2012-10-06 20:13:02 +0200

    it works bitches - rewritten from scratch

--- callgraph_diff.py
+++ callgraph_diff.py
@@ -2,31 +2,33 @@ from sets import Set
 
 # Get the segment's starting address
 ea = ScreenEA()
-callers = dict()
-callees = dict()
+funcA=0x408bab
+funcB=0x401730
 
-# Loop through all the functions
-for function_ea in Functions(SegStart(ea), SegEnd(ea)):
-    f_name = GetFunctionName(function_ea)
+callersA=Set()
+callersB=Set()
 
-    # Create a set with all the names of the functions calling (referring to)
-    # the current one.
-    callers[f_name] = Set(map(GetFunctionName, CodeRefsTo(function_ea, 0)))
+def GetRefToSet(to_func,s=Set()):
+    s.add(GetFunctionName(to_func))
+    for r in CodeRefsTo(to_func,1):
+        if GetFunctionName(r) not in s:
+            #print("%s @ %x" % (GetFunctionName(r),LocByName(GetFunctionName(r))))
+            GetRefToSet(LocByName(GetFunctionName(r)),s)
+    return s
 
-    # For each of the incoming references
-    for ref_ea in CodeRefsTo(function_ea, 0):
-        # Get the name of the referring function
-        caller_name = GetFunctionName(ref_ea)
+print "Caller list of function A:"
+GetRefToSet(funcA,callersA)
+print callersA
+print "Caller list of function B:"
+GetRefToSet(funcB,callersB)
+print callersB
 
-        # Add the current function to the list of functions
-        # called by the referring function
-        callees[caller_name] = callees.get(caller_name, Set())
-        callees[caller_name].add(f_name)
+print "Common callers:"
+print callersA & callersB
 
-# Get the list of all functions
-functions = Set(callees.keys()+callers.keys())
-# For each of the functions, print the number of functions calling it and
-# number of functions being called. In short, indegree and outdegree
-for f in functions:
-    print '%d:%s:%d' % (len(callers.get(f, [])), f, len(callees.get(f, [])))
+print "A-B:"
+print callersA - callersB
+print "B-A:"
+print callersB - callersA
+
 
